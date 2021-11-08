2054117c89260a536c95746d746668140bb6eb7d
budhitejaswi <87587371+budhitejaswi@users.noreply.github.com>
2021-11-07 19:28:25 -0600

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCfwzgO7R6mRFyrf4TQDD5aXKvPyPliFno",
+    authDomain: "let-s-chat-web-app-5c07a.firebaseapp.com",
+    projectId: "let-s-chat-web-app-5c07a",
+    storageBucket: "let-s-chat-web-app-5c07a.appspot.com",
+    messagingSenderId: "986308324723",
+    appId: "1:986308324723:web:f8018e604d016eb9a9a906"
+  };
+
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
